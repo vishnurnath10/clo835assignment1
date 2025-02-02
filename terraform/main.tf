e43ab260d38dfcb80eebe7e1e9782f53ec1d4a12
vishnurnath10 <vishnurnath367@gmail.com>
2025-02-02 03:50:40 +0000

    changed terraform code

--- terraform/main.tf
+++ terraform/main.tf
@@ -57,15 +57,13 @@ resource "aws_key_pair" "ssh_keypair" {
 resource "aws_security_group" "ec2_sg" {
   vpc_id = data.aws_vpc.default.id
 
-
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
- 
   ingress {
     from_port   = 80
     to_port     = 80
@@ -80,7 +78,27 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  
+  ingress {
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8082
+    to_port     = 8082
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8083
+    to_port     = 8083
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
